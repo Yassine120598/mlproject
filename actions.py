58383ecfcc6b6404286b74d2627fb7cebf286f37
Yassine120598 <70445067+Yassine120598@users.noreply.github.com>
2023-08-18 10:52:43 +0200

    Add files via upload

--- actions.py
+++ actions.py
@@ -1,26 +1,87 @@
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action
 import pandas as pd
+import matplotlib.pyplot as plt
 
-class ActionFetchBudget(Action):
+class ActionShowProjectBudget(Action):
+    def name(self):
+        return "action_show_project_budget"
 
-    def name(self) -> str:
-        return "action_fetch_budget"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list:
-        # Load the Excel file
-        budget_data = pd.read_excel("C:/Users/Yassine/Pictures/Budget/budget.xlsx")
-
-        # Get the project name from the tracker
-        project_name = tracker.get_slot("project")
-
-        # Check if the project is in the Excel file
-        if project_name in budget_data["Project"].values:
-            budget = budget_data[budget_data["Project"] == project_name]["Budget"].iloc[0]
-            dispatcher.utter_message(text=f"The budget for the project {project_name} is {budget}.")
-        else:
-            dispatcher.utter_message(text="Sorry, I don't have information about that project.")
+    def run(self, dispatcher, tracker, domain):
+        # Read the Excel file
+        budget_data = pd.read_excel("C:/Users/Yassine/Pictures/12/budgetM.xlsx")
         
-        return []
+        # Get the project name from the captured entity
+        project_name = tracker.get_slot('project')
+        
+        # Fetch the budget for this project
+        project_budget = budget_data[budget_data['Project'] == project_name]['Budget '].values[0]
+        
+        # Send the response
+        dispatcher.utter_message(f"The allocated budget for project {project_name} is {project_budget}.")
+
+class ActionShowOverconsumption(Action):
+    def name(self):
+        return "action_show_overconsumption"
+
+    def run(self, dispatcher, tracker, domain):
+        # Read the Excel file
+        budget_data = pd.read_excel("C:/Users/Yassine/Pictures/12/budgetM.xlsx")
+        
+        # Get the project name from the captured entity
+        project_name = tracker.get_slot('project')
+        
+        # Fetch the overconsumption for this project
+        overconsumption = budget_data[budget_data['Project'] == project_name]['overconsumption'].values[0]
+        
+        # Send the response
+        dispatcher.utter_message(f"The overconsumption for project {project_name} is {overconsumption}.")
+
+class ActionShowBudgetPerMonthHistogram(Action):
+    def name(self):
+        return "action_show_budget_per_month_histogram"
+
+    def run(self, dispatcher, tracker, domain):
+        # Read the Excel file
+        budget_data = pd.read_excel("C:/Users/Yassine/Pictures/12/budgetM.xlsx")
+        
+        # Aggregating the monthly budgets
+        monthly_budgets = budget_data[['Budget Per June', 'Budget per july', 'Budget per august ']].sum()
+        
+        # Plotting the histogram
+        monthly_budgets.plot(kind='bar')
+        plt.title('Budget Allocated Each Month')
+        plt.ylabel('Budget')
+        plt.xlabel('Month')
+        plt.tight_layout()
+        
+        # Save the figure
+        plt.savefig("C:/Users/Yassine/Pictures/12/monthly_budget_histogram.png")
+        
+        # Send the response (you might need to integrate a way to display the image in your chat interface)
+        dispatcher.utter_message(image="C:/Users/Yassine/Pictures/12/monthly_budget_histogram.png")
+
+class ActionShowMonthlyBudget(Action):
+    def name(self):
+        return "action_show_monthly_budget"
+
+    def run(self, dispatcher, tracker, domain):
+        # Read the Excel file
+        budget_data = pd.read_excel("C:/Users/Yassine/Pictures/12/budgetM.xlsx")
+        
+        # Get the project name from the captured entity
+        project_name = tracker.get_slot('project')
+        
+        # Fetch the monthly budget details for this project
+        project_data = budget_data[budget_data['Project'] == project_name]
+        budget_june = project_data['Budget Per June'].values[0]
+        budget_july = project_data['Budget per july'].values[0]
+        budget_august = project_data['Budget per august '].values[0]
+        
+        # Send the response
+        response = (f"The monthly budget distribution for project {project_name} is:\n"
+                    f"June: {budget_june}\n"
+                    f"July: {budget_july}\n"
+                    f"August: {budget_august}")
+        dispatcher.utter_message(response)
 
 
